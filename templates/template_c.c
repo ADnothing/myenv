b5166ab5e945ef9ac1ef2d52dad817e80de99e60
Adrien Anthore <75272960+ADnothing@users.noreply.github.com>
2023-04-12 13:44:40 +0200

    Upload templates
    Initial upload of the templates that works with the scripts

---
+++ templates/template_c.c
@@ -0,0 +1,19 @@
+/*
+Adrien Anthore, date
+
+Compilation :
+gcc -ansi -Wall -Wextra -o file.x file.c 
+Execution :
+./file.x
+*/
+
+int main(void)
+{
+/* VARIABLES */
+
+
+/* INSTRUCTIONS */
+
+
+return 0;
+}
---
+++ templates/template_py.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#Adrien Anthore, date
+#File.py
+#main python file
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
