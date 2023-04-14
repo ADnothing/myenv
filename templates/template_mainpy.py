0d529a9bac51afa97a052cf35de503d666918e56
Adrien Anthore <75272960+ADnothing@users.noreply.github.com>
2023-04-14 11:17:14 +0200

    Py tempaltes update
    See the update on crea_py :
    Added template_mainpy.py and updated template_py.py

---
+++ templates/template_mainpy.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+#USER, date
+#File.py
+#main python file
+
+
--- templates/template_py.py
+++ templates/template_py.py
@@ -2,9 +2,8 @@
 
 #USER, date
 #File.py
-#main python file
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
+
