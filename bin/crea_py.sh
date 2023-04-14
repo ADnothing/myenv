f0aa164b2f16acb093197b2feafd0ec2eb315ddc
Adrien Anthore <75272960+ADnothing@users.noreply.github.com>
2023-04-14 11:15:40 +0200

    crea_py updateted
    Now create python file and a main python file that include all the other files (if accepted).

--- bin/crea_py.sh
+++ bin/crea_py.sh
@@ -6,24 +6,50 @@ if [ $# -lt 1 ]; then
 fi
 
 today=$(date | awk '{print $2, $3, $4}')
+ans=""
+
+echo -n "Create a main python file ? (y/n) " ; read ans
+
+if [ "$ans" == y ]; then
+  if [ -f "main.py" ];  then
+    echo -e "ERROR : main.py already exist."
+    exit 2
+  fi
+  
+  sed -e "s/File/main/g" $HOME/Documents/templates/template_mainpy.py > temp1
+  sed -e "s/date/$today/g" temp1 > temp2
+  sed -e "s/USER/$USER/g" temp2 > main.py
+  
+  rm temp*
+fi
 
 for file in "$@"; do
   if [ -f "$file.py" ]; then
     echo -e "ERROR : $file.py already exist."
-    exit 2
+    exit 3
   fi
 
   sed -e "s/File/$file/g" $HOME/Documents/templates/template_py.py > "$file"_temp1
   sed -e "s/date/$today/g" "$file"_temp1 > "$file"_temp2
   sed -e "s/USER/$USER/g" "$file"_temp2 > "$file".py
 
-  rm "$file"_temp
+  rm "$file"_temp*
   
   if [ ! -f "$file.py" ]; then
     echo -e "ERROR: Failed to create $file.c"
-    exit 3
+    exit 4
+  fi
+  
+  if [ "$ans" == y ]; then
+    echo "from $file import *" >> main.py
   fi
 
 done
+
+if [ "$ans" == y ]; then
+  echo "" >> main.py
+  echo 'if __name__ == "__main__":' >> main.py
+  echo "" >> main.py
+fi
 
 exit 0
